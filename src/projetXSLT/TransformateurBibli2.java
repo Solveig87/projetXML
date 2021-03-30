514d06337fa5e976aaa88e004b9fe6e1f059af47
camillerey <camillerey@localhost>
2021-03-30 10:42:52 +0200

    ajout classe transformateur2

---
+++ src/projetXSLT/TransformateurBibli2.java
@@ -0,0 +1,66 @@
+package projetXSLT;
+
+import java.io.IOException;
+
+import javax.xml.transform.TransformerException;
+
+import org.apache.fop.apps.FOPException;
+import org.xml.sax.SAXException;
+
+public class TransformateurBibli2 {
+	
+	private String htmlStylesheet;
+	private String pdfStylesheet;
+	
+	public TransformateurBibli2() {
+		this.setHtmlStylesheet("to_html.xsl");
+		this.setPdfStylesheet("xmltopdf.xsl");
+	}
+	
+	public void convertXML(String input, String output, String format) {
+	    if (format.equals("pdf")) {
+	        try {
+	        	PdfGeneration.convertToPDF(input, pdfStylesheet, output);
+	        } catch (FOPException | IOException | TransformerException e) {
+	            e.printStackTrace();
+	        }
+		}
+		    
+	    else if (format.equals("html")) {
+	        try {
+	        	HtmlGeneration.convertToHtml(input, htmlStylesheet, output);
+	        } catch ( IOException | TransformerException | SAXException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    
+	    else {
+			System.out.println("ERREUR - Le format demandé n'est pas pris en charge.");
+		}
+	}
+
+	public String getHtmlStylesheet() {
+		return htmlStylesheet;
+	}
+
+	public void setHtmlStylesheet(String htmlStylsheet) {
+		this.htmlStylesheet = htmlStylsheet;
+	}
+
+	public String getPdfStylesheet() {
+		return pdfStylesheet;
+	}
+
+	public void setPdfStylesheet(String pdfStylesheet) {
+		this.pdfStylesheet = pdfStylesheet;
+	}	 
+	
+
+    public static void main( String[] args )
+    {
+    	TransformateurBibli2 transfo = new TransformateurBibli2();
+        transfo.convertXML("bibliotheque3.xml", "test4.html", "html");
+    }
+
+}
+
